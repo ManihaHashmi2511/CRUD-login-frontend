7c6870530b061fdae87ed9e9b4c5c409e7664989
ManihaHashmi2511 <it.solution8800@gmail.com>
2026-06-08 10:12:20 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Default page → Login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        {/* Public Routes */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Route → sirf logged in user */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
